678e05c580f5f577cf26af9a6cbda9d66da156e0
carlos calderon <cdcalderon@gmail.com>
2020-07-11 14:02:53 -0500

    add Row styling

--- src/Row.js
+++ src/Row.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "./axios";
+import "./Row.css";
+
 const base_url = "https://image.tmdb.org/t/p/original/";
 
 function Row({ title, fetchUrl }) {
@@ -21,7 +23,11 @@ function Row({ title, fetchUrl }) {
 
       <div className="row_posters">
         {movies.map(movie => (
-          <img src={`${base_url}${movie.poster_path}`} alt={movie.name} />
+          <img
+            className="row_poster"
+            src={`${base_url}${movie.poster_path}`}
+            alt={movie.name}
+          />
         ))}
       </div>
     </div>
