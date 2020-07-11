8438c6000fe9e0280f4b781a79cdbcc486b8e478
carlos calderon <cdcalderon@gmail.com>
2020-07-11 17:17:16 -0500

    implement banner

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from "react";
 import "./App.css";
 import Row from "./Row";
+import Banner from "./Banner";
 import requests from "./requests";
 
 function App() {
   return (
     <div className="App">
+      <Banner />
       <Row
         title="NETFLIX ORIGINALS"
         fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
       />
       <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
       <Row title="Top Rated" fetchUrl={requests.fetchTrending} />
