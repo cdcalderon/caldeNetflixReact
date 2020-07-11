2d6627cddbe70feb6d0d768c74901192472eb78b
carlos calderon <cdcalderon@gmail.com>
2020-07-11 13:36:56 -0500

    add movie poster container

--- src/Row.js
+++ src/Row.js
@@ -5,18 +5,24 @@ function Row({title, fetchUrl}) {
     const [movies, setMovies] = useState([]);
 
     useEffect(() => {
-
         async function fetchData() {
             const request = await axios.get(fetchUrl);
-            console.log(request);
+            setMovies(request.data.results)
             return request;
         }
         fetchData();
-    }, []);
+    }, [fetchUrl]);
 
+    console.table(movies);
     return (
-        <div>
+        <div className="row">
            <h2>{title}</h2>
+
+           <div className="row_posters">
+                {movies.map(movie => (
+                    <img src={movie.poster_path} alt={movie.name}/>
+                ))}
+           </div>
         </div>
     )
 }
